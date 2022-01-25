3aa23a1c878f3aeea5bc61a5646a5d186556bf79
Annabelle Whipps <49943973+annabelle-whipps@users.noreply.github.com>
2022-01-25 16:37:34 +0000

    Changes to index.js

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	
 	response.end('Hello World\n');
+	
+	const PORT = process.env.PORT || 3000;
+		app.listen(PORT, () => {
+		console.log('The app is runiing on port ${ PORT }');
 
-}).listen(3000);
+	}); 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log('The app is runiing on port ${ PORT }');
+})
+	
 	
